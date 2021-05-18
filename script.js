c52347201b75f55ff4748c58963dc4b734ab2175
Gustavo Rezende Melo <42256377+GustavoRMelo@users.noreply.github.com>
2021-05-18 12:31:31 -0300

    Resizeble Grid with limit
    Added a button that resizes the Grid with a limit of 100 squares. If the size is bigger, the grid stays the same.

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
 const container = document.querySelector('.grid');
-
-for (i=1; i<=16;i++){
-    const content = document.createElement('div');
-    content.classList.add('cell');
-    content.setAttribute('id', i);
-    content.textContent = i;
-    container.appendChild(content);
-    let test = document.getElementById(i);
-    test.addEventListener("mouseover", function( event ) {
-    // highlight the mouseover target
-    event.target.style.color = "orange";
+const inicialGrid = 16;
+createGrid(inicialGrid);
+document.getElementById("btn").addEventListener("click", changeGrid);
+function changeGrid(){
+    let gridSize = prompt("Choose grid size (limit=100): ")
+    if (gridSize>100){
+        alert("Grid too big.");
+        return false;
+    }
+    let gridCreated = Math.ceil(Math.sqrt(gridSize));
+    let columns = 'grid-template-columns: repeat('+gridCreated+',80px)';
+    //let rows = 'grid-template-rows: repeat('+gridCreated+',80px)';
+    removeAllChildNodes(container);
+    container.setAttribute('style', columns);
+    //container.setAttribute('style', rows);
+    createGrid(gridSize);
+};
+function createGrid(inicialGrid){
+    for (i=1; i<=inicialGrid;i++){
+        const content = document.createElement('div');
+        content.classList.add('cell');
+        content.setAttribute('id', i);
+        content.textContent = i;
+        container.appendChild(content);
+        let test = document.getElementById(i);
+        test.addEventListener("mouseover", function( event ) {
+        // highlight the mouseover target
+        event.target.style.color = "orange";
   
-    // reset the color after a short delay
-        setTimeout(function() {
-            event.target.style.color = "";
-        }, 500);
-    }, false);
+        // reset the color after a short delay
+            setTimeout(function() {
+                event.target.style.color = "";
+            }, 500);
+        }, false);
+    };
+};
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
 };
